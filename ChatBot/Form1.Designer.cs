0233dbe4033601c9b0321d3da70b0fc928f46410
hugo-lcm <hugolcm@live.com>
2022-08-03 14:31:26 -0300

    alteração no layout

--- ChatBot/Form1.Designer.cs
+++ ChatBot/Form1.Designer.cs
@@ -76,7 +76,7 @@
             // 
             this.button1.BackColor = System.Drawing.SystemColors.Control;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(76, 381);
+            this.button1.Location = new System.Drawing.Point(83, 381);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
